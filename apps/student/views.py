25d97aa4fe8408a41a137730bcf9e72f6187844b
Louis Agyapong <agyapong.louis@gmail.com>
2021-03-24 21:35:29 +0000

    pagination

--- apps/student/views.py
+++ apps/student/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 from .models import Student
 from .serializers import StudentSerializer
 
 
+class StudentPagination(PageNumberPagination):
+    page_size = 3
+
+
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    pagination_class = LimitOffsetPagination
