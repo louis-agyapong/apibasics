c7d765d56b284de8bc10d92bf20f8c1b2a4baf48
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-20 22:04:19 +0000

    url refactor

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
-from apps.polls.apiviews import PollDetail, PollList
+from apps.polls.apiviews import PollDetail, PollList, ChoiceList, CreateVote
 
 urlpatterns = [
-    path("", PollList.as_view(), name="polls_list"),
-    path("<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("polls/", include([
+        path("", PollList.as_view(), name="polls_list"),
+        path("<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+        path("choices/", ChoiceList.as_view(), name="choices_list"),
+        path("vote/", CreateVote.as_view(), name="create_vote"),
+
+    ]))
 ]
