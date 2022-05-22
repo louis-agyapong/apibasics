d117599635f41c23087a522351eef5369ef5ed6e
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-22 23:42:53 +0000

    createvote serializer function

--- apps/polls/apiviews.py
+++ apps/polls/apiviews.py
@@ -26,7 +26,6 @@ class ChoiceList(generics.ListCreateAPIView):
 
 
 class CreateVote(APIView):
-
     def post(self, request, pk, choice_pk):
         voted_by = request.data.get("voted_by")
         data = {"choice": choice_pk, "poll": pk, "voted_by": voted_by}
@@ -35,6 +34,4 @@ class CreateVote(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
-
-
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
